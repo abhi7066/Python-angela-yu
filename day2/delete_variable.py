8154b0c798a2a3f95ce17dbe33acf4d59483cf2b
Abhishek Dethe <abhishekdethe7066@gmail.com>
2023-04-23 02:23:57 +0530

    added some file

---
+++ day2/delete_variable.py
@@ -0,0 +1,7 @@
+name ="Abhishek" 
+
+print(name) #This will print name = Abhishek
+
+del name   #This function will delete variable reference
+
+print(name) #gives error because we have deleted the variable name
---
+++ day2/multipleAssignment.py
@@ -0,0 +1,21 @@
+#Multiple Assignment
+
+a = b = c = 100
+
+print (a)
+print (b)
+print (c)
+
+# another step
+a,b,c = 1,2,"Zara Ali"
+
+print (a)
+print (b)
+print (c)
+
+
+# Unpack a list
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x,y,z)
