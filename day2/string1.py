5e2e9aa4f73cf5e54a66068cc27b167729ad952d
Abhishek Dethe <abhishekdethe7066@gmail.com>
2023-04-22 23:58:49 +0530

    added day2

---
+++ day2/string1.py
@@ -0,0 +1,26 @@
+#Data Types in Python
+#String
+print("Hello"[0])
+# This will print 0th index of string
+
+print("Hello"[4])
+# This will print last element
+
+# cannot concat string with integer
+
+#Integer
+print(12+12)
+#This will return 24
+
+print(123_213_123)
+#This is just for our understanding 123,213,123
+
+#Float
+print(765.87)
+
+
+#Boolean
+True
+False
+
+print(True)
