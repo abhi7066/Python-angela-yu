0a00e4a299464367e378fcde74293c72c3f9449b
Abhishek Dethe <abhishekdethe7066@gmail.com>
2023-04-22 14:11:46 +0530

    added string concat file

---
+++ day1/strconcat.py
@@ -0,0 +1,7 @@
+print("Welcome to Band Name Generator")
+
+city = input("which city did you grow up?\n")
+
+pet = input("What is the name of pet?\n")
+
+print("Your band could be " + city + " " + pet)
